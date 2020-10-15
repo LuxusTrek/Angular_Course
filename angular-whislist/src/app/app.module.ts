2a7c871cf3be390a0b48827c06d01a754d8067dd
Luis Carlos Camacho Rodriguez <luiscamacho20000@gmail.com>
2020-10-15 00:21:51 -0500

    Componente de Lista de viajes

---
+++ angular-whislist/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DestinoComponent } from './destino/destino.component';
+import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DestinoComponent,
+    ListaDestinosComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
